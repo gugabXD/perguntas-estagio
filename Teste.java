2a9920d5cadb35c7ded369bf7f1a7dc50ae50575
gustavo <gustavobpasquali2@gmail.com>
2024-03-06 04:19:54 -0300

    resposta das questoes 2 e 5

--- Teste.java
+++ Teste.java
@@ -1,18 +1,37 @@
 
+import java.util.ArrayList;
 public class Teste{
 public static void main(String args[]){
-        int INDICE = 13, SOMA = 0, K = 0;
+   System.out.println(questao2(1597));
+   System.out.println(questao2(1598));
 
-    while(K < INDICE)
+   System.out.println(questao5("batata frita"));;
+}
 
-    {
-
-    K = K + 1;
-
-    SOMA = SOMA + K;
-
+public static boolean questao2(int n){
+    int a = 0; int b = 1; int c = 1;
+    ArrayList<Integer> lista = new ArrayList<Integer>();
+    lista.add(a);
+    lista.add(b);
+    lista.add(c);
+    while(c<n){
+        a = b;
+        b = c;
+        c = a+b;
+        lista.add(c);
     }
+    if(lista.contains(n)) return true;
+    return false;
+}
 
-    System.out.println(SOMA);;
+public static String questao5(String s){
+    int tamanho = s.length();
+    String res = "";
+    for(int i=tamanho-1; i>=0; i--){
+        char c = s.charAt(i);
+        res+=c;
+    }
+    return res;
+}
+}
 
-}}
